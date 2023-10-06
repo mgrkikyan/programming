eb7a03e742434846264472d141d11ee421ed3ae6
mgrkikyan <mgrkikyan@list.ru>
2023-10-06 19:51:12 +0400

    add

--- lab1/app.c
+++ lab1/app.c
@@ -1,15 +1,22 @@
+// MEDIUM уровень
+
 #include <stdio.h>
 
 int main()
 {
     int x;
+    FILE *file = fopen("logfile", "a");  // Открытие файла log.txt в режиме "дописывание в конец"
+    if (file == NULL) {
+        printf("Oshibka!\n");
+        return 1;
+    }
     while (1) 
         {
         printf("Enter x -> ");
         scanf("%d", &x);
         if (x == 0)
         break;
-        printf("You entered: %d\n", x);
+        printf("Vi vveli: %d\n", x);
         }
-printf("Exiting the loop.");
+printf("Vi vveli 0.");
 }
